096bdf2f23219203d28df16b30869dd9cb862ba1
Kristian Monsen Haug <kristian.haug95@gmail.com>
2021-07-23 00:43:15 +0200

    Map to correct Chip 8 chars

--- run.cpp
+++ run.cpp
@@ -32,22 +32,22 @@ int main(int argc, char* argv[])
 
 
         std::unordered_map<int,char> scan_map = {
-            {SDL_SCANCODE_1, '1'},
+            {SDL_SCANCODE_1, '1'}, 
             {SDL_SCANCODE_2, '2'},
             {SDL_SCANCODE_3, '3'},
-            {SDL_SCANCODE_4, '4'},
-            {SDL_SCANCODE_Q, 'q'},
-            {SDL_SCANCODE_W, 'w'},
-            {SDL_SCANCODE_E, 'e'},
-            {SDL_SCANCODE_R, 'r'},
-            {SDL_SCANCODE_A, 'a'},
-            {SDL_SCANCODE_S, 's'},
-            {SDL_SCANCODE_D, 'd'},
-            {SDL_SCANCODE_F, 'f'},
-            {SDL_SCANCODE_Z, 'z'},
-            {SDL_SCANCODE_X, 'x'},
-            {SDL_SCANCODE_C, 'c'},
-            {SDL_SCANCODE_V, 'v'}
+            {SDL_SCANCODE_4, 'C'},
+            {SDL_SCANCODE_Q, '4'},
+            {SDL_SCANCODE_W, '5'},
+            {SDL_SCANCODE_E, '6'},
+            {SDL_SCANCODE_R, 'D'},
+            {SDL_SCANCODE_A, '7'},
+            {SDL_SCANCODE_S, '8'},
+            {SDL_SCANCODE_D, '9'},
+            {SDL_SCANCODE_F, 'E'},
+            {SDL_SCANCODE_Z, 'A'},
+            {SDL_SCANCODE_X, '0'},
+            {SDL_SCANCODE_C, 'B'},
+            {SDL_SCANCODE_V, 'F'}
         };
 
 

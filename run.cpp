cbedaf00aabc481e5c2a24da9967d8fde9699c29
Kristian Monsen Haug <kristian.haug95@gmail.com>
2021-07-25 21:51:13 +0200

    Render sprites

--- run.cpp
+++ run.cpp
@@ -1,7 +1,50 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <unordered_map>
+#include <array>
+#include <iomanip>
 
+
+void render_symbol(SDL_Renderer* renderer, char symbol) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    const std::unordered_map<char,std::array<uint16_t,5>> sprites = {
+        { '0', { 0xF0, 0x90, 0x90, 0x90, 0xF0 }},
+        { '1', { 0x20, 0x60, 0x20, 0x20, 0x70 }},
+        { '2', { 0xF0, 0x10, 0xF0, 0x80, 0xF0 }},
+        { '3', { 0xF0, 0x10, 0xF0, 0x10, 0xF0 }},
+        { '4', { 0x90, 0x90, 0xF0, 0x10, 0x10 }},
+        { '5', { 0xF0, 0x80, 0xF0, 0x10, 0xF0 }},
+        { '6', { 0xF0, 0x80, 0xF0, 0x90, 0xF0 }},
+        { '7', { 0xF0, 0x10, 0x20, 0x40, 0x40 }},
+        { '8', { 0xF0, 0x90, 0xF0, 0x90, 0xF0 }},
+        { '9', { 0xF0, 0x90, 0xF0, 0x10, 0xF0 }},
+        { 'A', { 0xF0, 0x90, 0xF0, 0x90, 0x90 }},
+        { 'B', { 0xE0, 0x90, 0xE0, 0x90, 0xE0 }},
+        { 'C', { 0xF0, 0x80, 0x80, 0x80, 0xF0 }},
+        { 'D', { 0xE0, 0x90, 0x90, 0x90, 0xE0 }},
+        { 'E', { 0xF0, 0x80, 0xF0, 0x80, 0xF0 }},
+        { 'F', { 0xF0, 0x80, 0xF0, 0x80, 0x80 }}};
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    auto sprite = sprites.at(symbol);
+    for (int row=0; row<sprite.size(); ++row) {
+        auto cursor = 0x80;
+        const auto x = sprite[row];
+        for (int col=0; col<8; ++col) {
+            if ((cursor & x) == cursor) {
+                SDL_RenderDrawPoint(renderer, col, row);
+            } 
+            cursor >>= 1;
+        }
+    }
+
+    
+
+    SDL_RenderPresent(renderer);
+
+}
 
 
 int main(int argc, char* argv[])
@@ -30,8 +73,6 @@ int main(int argc, char* argv[])
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 100);
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 255,255, 255, 255);
-
-        SDL_RenderDrawPoint(renderer, 0, 0);
 
         SDL_RenderPresent(renderer);
 
@@ -69,8 +110,11 @@ int main(int argc, char* argv[])
                 // Handle keypresses
                 case SDL_KEYDOWN:
                     auto sc_code = event.key.keysym.scancode;
-                    if (scan_map.find(sc_code) != scan_map.end())
-                        std::cout << scan_map[sc_code] << '\n';
+                    if (scan_map.find(sc_code) != scan_map.end()) {
+                        auto symbol = scan_map[sc_code];
+                        std::cout << symbol << '\n';
+                        render_symbol(renderer, symbol); 
+                    }
                 }
             }
         }
